76899b9eae133d84a98d4e774c61bde1ee566554
lolita <bkhit.amine.solicode@gmail.com>
2023-06-03 18:37:43 +0100

    ProfileImageController ADED
    now i can uplad the image inside a public storage path
    and return the url to the frond end
    also update the user profileImage column
    
    more fixes needed
    like remeve the old image if it not == the default profile app image
    add authorization
    and add resources to the /user response so the front end will get full url not only a path :100:

---
+++ app/Http/Controllers/API/v1/profileImageController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Controllers\API\v1;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Storage;
+use App\Http\Requests\ProfileImageRequest;
+
+class profileImageController extends Controller
+{
+
+
+
+    public function upload(ProfileImageRequest $request)
+    {
+
+
+
+        if ($request->hasFile('ProfileAvatar')) {
+            $file = $request->file('ProfileAvatar');
+
+            // Generate a unique name for the file
+            $filename = uniqid() . '.' . $file->getClientOriginalExtension();
+
+            // Store the file using the Storage facade
+            $path = Storage::disk('public')->putFileAs('uploads/images/profileImages', $file, $filename);
+
+            //update the user ProfileImage in DB
+            $request->user()->update(['profileImage' => $path]);
+
+            // Retrieve the public URL for the uploaded file
+            $url = Storage::disk('public')->url($path);
+            // Perform any additional logic with the file or its URL
+
+            return response()->json(['url' => $url]);
+        }
+
+        return "";
+
+
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\API\v1\profileImageController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\API\v1\UserLogin;
@@ -32,6 +33,8 @@ Route::prefix('v1')->group(function () {
 
         Route::post('/logout', [UserLogout::class, 'logout']);
 
+        // ROUTE used by FilePond to Upload profile image  #needs Authorization
+        Route::post('/profileImage', [profileImageController::class, 'upload']);
 
     });
 
@@ -39,5 +42,6 @@ Route::prefix('v1')->group(function () {
 
     Route::post('/login', [UserLogin::class, 'login']);
     Route::post('/register', [UserRegister::class, 'register']);
+
 
 });
